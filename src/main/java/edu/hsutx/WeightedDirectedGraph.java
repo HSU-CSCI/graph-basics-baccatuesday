cbaba77b3c76fdc6f3e05c21f336b6dcf5b5f12e
Bay Tompkins <orangeessence01@gmail.com>
2025-10-13 11:03:48 -0500

    getBFSpath

--- src/main/java/edu/hsutx/WeightedDirectedGraph.java
+++ src/main/java/edu/hsutx/WeightedDirectedGraph.java
@@ -4,25 +4,36 @@ import java.util.List;
 
 public class WeightedDirectedGraph {
     // TODO - Implement storage of the graph, including either adjacency list, adjacency matrix, or both.
+    // Going with adjacency matrix
+    // hold the edge weights and vertex count
+    private static double[][] adjMatrix;
+    private static int verCount;
 
     /***
+     * fills the adjacency matrix with vertices and their weights.
      *
      * @param vertexQuantity: Total number of vertices, as an int.  We will start counting at vertex 1, not 0.
      * @param edgeList: an List of Edges containing start and end vertex # and weight.
      ***/
     public WeightedDirectedGraph(int vertexQuantity, List<Edge> edgeList) {
-        // TODO - Implement
+        // create matrix and keep track of vertices
+        adjMatrix = new double[vertexQuantity + 1][vertexQuantity + 1];
+        verCount = vertexQuantity;
 
+        // loop through edgeList and put start, end, weight into Matrix
+        for (Edge edge : edgeList) {
+            adjMatrix[edge.getStart()][edge.getEnd()] = edge.getWeight();
+        }
     }
 
     /***
      * returns true if vertex[start] has an edge to vertex[end], otherwise returns false
-     * @param start
-     * @param end
+     * @param start starting vertex
+     * @param end ending vertex
      */
     public static boolean isAdjacent(int start, int end) {
-        // TODO - Implement
-        return true;
+        // 0 is no edge, since vertex count starts at 1
+        return adjMatrix[start][end] != 0;
     }
 
     /***
@@ -30,8 +41,7 @@ public class WeightedDirectedGraph {
      * @return matrix of doubles representing adjacent edge weights
      */
     public static double[][] adjacencyMatrix() {
-        // TODO - Implement
-        return null;
+        return adjMatrix;
     }
 
     /***
@@ -42,7 +52,82 @@ public class WeightedDirectedGraph {
      * @return an array of integers containing the path of vertices to be traveled, including start and end.
      */
     public static int[] getBFSPath(int start, int end) {
-        // TODO - Implement
+
+        // array to keep track of vertices visited
+        boolean[] visited = new boolean[verCount + 1];
+
+        // array to keep track of the path taken
+        // initialize all as zero
+        int road[] = new int[verCount+1];
+        for (int i = 0; i <= verCount; i++) {
+            road[i] = 0;
+        }
+
+        // create queue for next vertex to visit
+        // establish front and back of the line
+        int[] queue = new int[verCount];
+        int front = 0;
+        int back = 0;
+
+        // start the queue by inserting the first vertex
+        // move the back pointer to the next available spot
+        // mark down first vertex as visited
+        queue[back] = start;
+        back++;
+        visited[start] = true;
+
+        // loop until Queue is empty
+        while (front != back) {
+            // read the first element of the queue
+            // then pop it
+            int current = queue[front];
+            front++;
+
+            // check if current is the end
+            // return path taken
+            if (current == end) {
+                // create an array big enough to hold the path
+                // record how many vertices are in trace
+                int[] tracePath = new int[verCount];
+                int pathLength = 0;
+
+                // reconstruct the path taken
+                while (current != start) {
+                    // record path at the current vertex
+                    // update number of vertices traced back
+                    // update current to previous vertex
+                    tracePath[pathLength] = current;
+                    pathLength++;
+                    current = road[current];
+                }
+                // add start to the traced path
+                tracePath[pathLength] = start;
+
+                // create path array
+                // loop through path length and reverse the order
+                int[] path = new int[pathLength + 1];
+                for (int i=0; i<= pathLength; i++) {
+                    path[i] = tracePath[pathLength - i];
+                }
+
+                return path;
+            }
+
+            // check all vertices for an edge
+            for (int i = 1; i <= verCount; i++) {
+                // check if a vertex is an edge and
+                // if it has not been visited before
+                if (isAdjacent(current, i) && !visited[i]) {
+                    // record that i-vertex has been visited
+                    // record the path and add vertex to the queue
+                    visited[i] = true;
+                    road[i] = current;
+                    queue[back] = i;
+                    back ++;
+                }
+            }
+        }
+
         return null;
     }
 
