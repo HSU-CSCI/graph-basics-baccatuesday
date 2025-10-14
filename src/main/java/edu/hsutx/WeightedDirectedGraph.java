54afaf7db235c2bee85523b991e74254e48c3a6b
Bay Tompkins <orangeessence01@gmail.com>
2025-10-14 17:46:22 -0500

    DFS is done, program complete, all 4 tests pass

--- src/main/java/edu/hsutx/WeightedDirectedGraph.java
+++ src/main/java/edu/hsutx/WeightedDirectedGraph.java
@@ -139,9 +139,83 @@ public class WeightedDirectedGraph {
      * @return an array of integers containing the path of vertices to be traveled, including start and end.
      */
     public static int[] getDFSPath(int start, int end) {
-        // TODO - Implement
+
+        // array to keep track of vertices visited
+        boolean[] visited = new boolean[verCount + 1];
+
+        // array to keep track of the path taken
+        // initialize all as zero
+        int road[] = new int[verCount+1];
+        for (int i = 0; i <= verCount; i++) {
+            road[i] = 0;
+        }
+
+        // create stack for next vertex to visit
+        // establish top of the stack
+        int[] stack = new int[verCount];
+        int top = 0;
+
+        // start the stack by inserting the first vertex
+        // mark down first vertex as visited
+        stack[top] = start;
+        top++;
+        visited[start] = true;
+
+
+        // loop until stack is empty
+        while (top != 0) {
+            // read the top element of the stack
+            // pop it
+            top--;
+            int current = stack[top];
+
+            // check if current is the end
+            // return path taken
+            if (current == end) {
+                // create an array big enough to hold the path
+                // record how many vertices are in trace
+                int[] tracePath = new int[verCount];
+                int pathLength = 0;
+
+                // reconstruct path taken
+                while (current != start) {
+                    // record path at the current vertex
+                    // update number of vertices traced back
+                    // update current to previous vertex
+                    tracePath[pathLength] = current;
+                    pathLength++;
+                    current = road[current];
+                }
+                // add start to the traced path
+                tracePath[pathLength] = start;
+
+                // create path array
+                // loop through path length and reverse the order
+                int[] path = new int[pathLength + 1];
+                for (int i=0; i<= pathLength; i++) {
+                    path[i] = tracePath[pathLength - i];
+                }
+
+                return path;
+            }
+
+            // check all vertices for an edge
+            for (int i = 1; i <= verCount; i++) {
+                // check if a vertex is an edge and
+                // if it has not been visited before
+                if (isAdjacent(current, i) && !visited[i]) {
+                    // record that i-vertex has been visited
+                    // record the path and add vertex to the stack
+                    visited[i] = true;
+                    road[i] = current;
+                    stack[top] = i;
+                    top++;
+                }
+            }
+
+        }
+
         return null;
     }
-
 }
 
